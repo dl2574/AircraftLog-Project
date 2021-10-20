92e93cbca9c868e45b0743ea74e4372185b820a4
lococo <20144415+dl2574@users.noreply.github.com>
2021-10-19 21:29:42 -0700

    Update secret

--- AircraftLog/settings.py
+++ AircraftLog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secret import *
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nvwtbp2v5+(v!-5ag$^mdv(8wlaf8i3q%o-i)1tboj#euilx=r'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
